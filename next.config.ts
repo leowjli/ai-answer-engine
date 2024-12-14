fc9dc6915ca08c6970d51f7ca70f4b773939641e
leowjli <leo.wj.li@gmail.com>
2024-12-14 02:23:59 -0800

    ignoring source map files for prod

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  productionBrowserSourceMaps: false,
+  webpack(config) {
+    // Ignore `.map` files during the build process
+    config.module.rules.push({
+      test: /\.map$/,
+      use: 'ignore-loader',
+    });
+
+    return config;
+  },
+
 };
 
 export default nextConfig;
